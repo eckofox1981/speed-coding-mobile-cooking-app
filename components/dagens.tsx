c2267a35ec751d3a6003e07825ad527060aa5a1b
eckofox1981 <eckofox1981@pm.me>
2025-09-24 23:45:35 +0200

    chore: removed logging

--- components/dagens.tsx
+++ components/dagens.tsx
@@ -4,9 +4,6 @@ import { ReceptCard } from "./recept-card";
 
 export function TodaysLunchSection() {
   const random = Math.floor(Math.random() * (19 - 0) + 0);
-  console.log("====================================");
-  console.log(random);
-  console.log("====================================");
 
   return (
     <View>
