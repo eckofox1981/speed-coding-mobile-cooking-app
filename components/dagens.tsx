c02d8b1c972e5996b993155862f6f551002f02db
eckofox1981 <eckofox1981@pm.me>
2025-09-24 23:43:31 +0200

    feat: randomizong today's featured lunch

--- components/dagens.tsx
+++ components/dagens.tsx
@@ -3,12 +3,15 @@ import { Text, View } from "react-native";
 import { ReceptCard } from "./recept-card";
 
 export function TodaysLunchSection() {
-  let random = recipes[0];
+  const random = Math.floor(Math.random() * (19 - 0) + 0);
+  console.log("====================================");
+  console.log(random);
+  console.log("====================================");
 
   return (
     <View>
       <Text> Todays Featured Lunch</Text>
-      <ReceptCard recept={random} />
+      <ReceptCard recept={recipes[random]} />
     </View>
   );
 }
